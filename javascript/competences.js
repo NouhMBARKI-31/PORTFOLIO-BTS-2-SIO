a69cc631400192917ec27f2096fddeb5c6065f4a
Mbarki Nouh <nouhmbarki31@gmail.com>
2025-04-11 11:33:03 +0200

    css modifié

--- javascript/competences.js
+++ javascript/competences.js
@@ -2,7 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Afficher la date dynamique
     const dateElement = document.getElementById("dateHeure");
     const now = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      };
+      
     dateElement.textContent = now.toLocaleDateString('fr-FR', options);
 
     // Bouton retour en haut
